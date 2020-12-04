a6049baa48f2b0e682623cb12a47145fc1d7da51
Greyson Kirk <greysonkirk@yahoo.com>
2020-12-03 19:34:20 -0600

    update employee done

--- employee.js
+++ employee.js
@@ -141,39 +141,55 @@ function updateEmp() {
                 message: "Which employee would you like to update?"
             }])
             .then(function(answer) {
-                console.log("\x1b[31m", answer.employee.first_name + " " + answer.employee.last_name + " " + answer.employee.id)
-                connection.query("SELECT * FROM employee where ?", {
-                        id: answer.employee.id
-                    },
-                    function(err, response) {
+                console.log("UPDATING: " + answer.employee.first_name + " " + answer.employee.last_name + " ID: " + answer.employee.id)
+                connection.query("SELECT * FROM role",
+                    function(err, roles) {
                         if (err) throw err;
-                        console.table(response)
+                        //console.table(response)
+                        inquirer
+                            .prompt([{
+                                name: "empRole",
+                                type: "list",
+                                choices: roles.map(role => ({ name: role.id + " " + role.title, value: role.id })),
+                                message: "What is the employees role?"
+                            }]).then(response => {
+                                console.log(response.empRole);
+                                connection.query("UPDATE employee SET ? WHERE ?", [{
+                                        role_id: response.empRole
+                                    },
+                                    {
+                                        id: answer.employee.id
+                                    }
+                                ], function() {
+                                    if (err) throw err;
+                                    console.log("role recived")
+                                })
+                            }).then(response => {
+                                connection.query("SELECT * FROM employee WHERE is_manager = 1", function(err, managers) {
+                                    if (err) throw err;
+                                    inquirer
+                                        .prompt([{
+                                            name: "empManager",
+                                            type: "list",
+                                            choices: managers.map(manager => ({ name: manager.first_name + " " + manager.last_name, value: manager.id })),
+                                            message: "What is the employees manager?"
+                                        }]).then(setMan => {
+                                            connection.query("UPDATE employee SET ? WHERE ?", [{
+                                                manager_id: setMan.empManager
+                                            }, {
+                                                id: answer.employee.id
+                                            }], function(err, response) {
+                                                if (err) throw err;
+                                                console.log("Employee has been updated!!!")
+                                            });
+                                        })
+                                })
+                            })
+                    });
 
 
-                        // inquirer
-                        //     .prompt([{
-                        //         name: "empManager",
-                        //         type: "list",
-                        //         choices: managers.map(manager => ({ name: manager.first_name + " " + manager.last_name, value: manager.id })),
-                        //         message: "What is the employees manager?"
-                        //     }]).then(function(response) {
-                        //         console.log(answer.empRole)
 
-                        //         connection.query("INSERT INTO employee SET ?", {
-                        //                 first_name: answer.empFirst,
-                        //                 last_name: answer.empLast,
-                        //                 role_id: answer.empRole,
-                        //                 manager_id: response.empManager
-                        //             },
-                        //             function(err, response) {
-                        //                 if (err) throw err;
-                        //                 console.log("Employee succesfully added!!!")
-                        //             }
 
-                        //         )
-                        //     });
-
-                    })
 
             })
     });
